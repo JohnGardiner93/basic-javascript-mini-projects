f1141f0e88374170091de32e33fd230acfe558d7
John Gardiner <JohnGardiner93@users.noreply.github.com>
2021-03-08 13:15:12 -0500

    To-Do-App: Text Input Validation Fix
    Fixed textInput validation in fxn addTasktoPage

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -73,7 +73,7 @@ const createTaskRowElement = function (taskElement) {
 // Page Functions
 const addTasktoPage = function () {
   // Validate input
-  if (!inputText.value) {
+  if (inputText.value !== '') {
     // Create task object
     const task = {
       taskDescription: inputText.value,
