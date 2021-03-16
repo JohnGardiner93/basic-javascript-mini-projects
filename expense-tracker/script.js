bd08ba9cb6d85573afb50abc26b9be17a885ac29
John Gardiner <JohnGardiner93@users.noreply.github.com>
2021-03-15 20:55:36 -0400

    Expense Tracking - Classes, Functions
    Started added classes, functions, for expense tracker. Added variables for major page elements. Changed feature requirements to have single delete button for each row as opposed to checkbox setup. Adjusted CSS for expense table.

--- expense-tracker/script.js
+++ expense-tracker/script.js
@@ -18,7 +18,6 @@ Page-Setup:
 ✅ - Amount input
 ✅ - Submit button
 ✅ - Clear form button
-✅ - Delete button
 ✅ - Table for expenses
 ✅ - Expense row in table
 ✅ - Checkbox to delete row
@@ -29,20 +28,60 @@ Program Flow:
 3. Information is validated in some fashion
 4. Expenses added to expenses array
 5. Expenses are displayed on page
-6. User can delete expenses one at a time or multiple at once.
-7. User can clear form at any point using a "Clear" button
-
+6. User can delete expenses one at a time
+7. User can clear form at any point using a "Clear Form" button
 */
 
 ////////////////////////////////////////////
 // Variables
+const inputDescription = document.getElementsByName(`Expense Description`);
+const inputAmount = document.getElementsByName(`Expense Amount`);
+const inputDate = document.getElementsByName(`Expense Date`);
+const btnSubmit = document.querySelector(`.btn--submit`);
+const btnClearForm = document.querySelector(`.btn--clear-form`);
+const tableBody = document
+  .querySelector(`.expense-table`)
+  .getElementsByTagName(`tbody`)[0];
+
+let idSet = Array(100)
+  .fill()
+  .map((_, index) => index);
+////////////////////////////////////////////
+// Class declarations
+// Expenses
+class Expense {
+  #id;
+  #description;
+  #amount;
+  #date;
+
+  constructor(description, amount, date) {
+    this.#description = description;
+    this.#amount = amount;
+    this.#date = date;
+    this.#id = idSet.shift();
+  }
+}
 
 ////////////////////////////////////////////
 // Functions
+const removeExpense = function (id) {
+  document.getElementById(id).remove();
+};
+
+const clearForm = function () {};
 
 ////////////////////////////////////////////
 // Event Listeners
+btnClearForm.addEventListener(`click`, clearForm);
 
 ////////////////////////////////////////////
 // Initialize
 console.log(`Expense Tracker`);
+
+////////////////////////////////////////////
+// Testing
+const testExpense = new Expense(`Test`, 40.0, `4 / 3 / 06`);
+const testExpense2 = new Expense(`Test 2`, 40.0, `4 / 3 / 06`);
+console.log(testExpense);
+console.log(testExpense2);
